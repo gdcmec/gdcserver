f7756eec86c75b15ecf3a51fa2935e833ca5f595
vaidyanath b <b.vaidyanath003@gmail.com>
2023-03-15 16:42:45 +0530

    mongoose setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const express = require("express")
+const mongoose = require("mongoose")
 require("dotenv").config()
 
 const port = process.env.PORT || 3000
 
 const app = express()
 
+mongoose.connect(`${process.env.MONGODB_URI}`);
+
+mongoose.connection.on('connected', () => {
+    console.log("mongoose connected");
+}) 
+
+app.use(express.json())
+//body parser
+app.use(express.urlencoded({ extended: true }))
+
 app.use("/cms", require("./routes/cms.route"))
+app.use("/events", require("./routes/events.route"))
 
 app.get("/", (req, res) => {
   res.send("GDC Server")
